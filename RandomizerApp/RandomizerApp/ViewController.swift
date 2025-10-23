22c515c7115337bd5ad6531a089169ca48e3a180
Arajlym Kabykenova <arailym.kabykenova@icloud.com>
2025-10-23 13:50:12 +0500

    changed

--- RandomizerApp/RandomizerApp/ViewController.swift
+++ RandomizerApp/RandomizerApp/ViewController.swift
@@ -13,10 +13,17 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var flowerImage: UIImageView!
     
+    @IBOutlet weak var flowerDiscription: UILabel!
+    
     @IBAction func selectButton(_ sender: UIButton) {
-        let flowers=["amaryllis","lily","poppy","rose","carnation","chrysanthemum","daisy","pansy","tulip","hibiscus"]
-        let flower=flowers.randomElement()!
-        flowerImage.image=UIImage(named:flower)
+        let flowersAndMood:[String:String]=["amaryllis":"Amaryllis — bold and confident","daisy":"Daisy — cheerful and pure","rose":"Rose — loving and passionate","tulip1":"Tulip — hopeful and warm","lily":"Lily — calm and graceful","poppy":"Poppy — creative and dreamy","carnation":"Carnation - gentle and inspired","chrysanthemum":"Chrysanthemum - wise and thoughtful","pansy":"Pansy — kind and reflective","hibiscus":"Hibiscus — vibrant and free-spirited"]
+        let flower=flowersAndMood.keys.randomElement()!
+        UIView.animate(withDuration: 0.6, animations: {
+            self.flowerImage.transform=self.flowerImage.transform.rotated(by:.pi )
+        },completion:{ _ in
+            self.flowerImage.image=UIImage(named:flower)})
+        //flowerImage.image=UIImage(named:flower)
+        flowerDiscription.text=flowersAndMood[flower]
         
     }
     override func viewDidLoad() {
